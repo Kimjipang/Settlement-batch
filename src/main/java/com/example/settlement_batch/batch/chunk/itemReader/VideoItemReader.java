5695c35ed7c54e7a754d40ef83fe26dfdff99525
김지환 <91196025+Kimjipang@users.noreply.github.com>
2024-08-03 13:00:25 +0900

    refactor : PagingItemReader에서 fetch join으로 데이터 조회

--- src/main/java/com/example/settlement_batch/batch/chunk/itemReader/VideoItemReader.java
+++ src/main/java/com/example/settlement_batch/batch/chunk/itemReader/VideoItemReader.java
@@ -8,7 +8,6 @@ import org.springframework.batch.item.database.JpaPagingItemReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-
 @Component
 @RequiredArgsConstructor
 public class VideoItemReader  {
@@ -17,12 +16,14 @@ public class VideoItemReader  {
     @Bean
     @StepScope
     public JpaPagingItemReader<Video> jpaPagingItemReader() {
+
         JpaPagingItemReader<Video> reader = new JpaPagingItemReader<>();
         reader.setEntityManagerFactory(entityManagerFactory);
-        reader.setQueryString("SELECT v FROM Video v JOIN FETCH v.user ORDER BY v.id"); // 이게 필수..?
-//        reader.setQueryString("SELECT v FROM Video v");
-        reader.setPageSize(10); // 원하는 페이지 크기로 설정
+        reader.setQueryString("SELECT v FROM Video v JOIN FETCH v.user ORDER BY v.id");
+//        reader.setQueryString("SELECT v FROM Video v ORDER BY v.id");
+        reader.setPageSize(10);
         reader.setSaveState(false);
         return reader;
     }
+
 }

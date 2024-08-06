bf47d91807f493d957c2f04f7716714f8168406f
김지환 <91196025+Kimjipang@users.noreply.github.com>
2024-08-06 21:39:27 +0900

    refactor: thread-safe한 writer로 변경

--- src/main/java/com/example/settlement_batch/batch/chunk/itemWriter/VideoStatisticsWriter.java
+++ src/main/java/com/example/settlement_batch/batch/chunk/itemWriter/VideoStatisticsWriter.java
@@ -1,11 +1,8 @@
 package com.example.settlement_batch.batch.chunk.itemWriter;
 
 import com.example.settlement_batch.video.entity.VideoStatistics;
-import com.example.settlement_batch.video.repository.write.VideoStatisticsWriteRepository;
 import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.item.Chunk;
-import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JpaItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -14,21 +11,10 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class VideoStatisticsWriter {
 
-    private final EntityManagerFactory entityManagerFactory;
-
     @Bean
     public JpaItemWriter<VideoStatistics> videoStatisticsJpaItemWriter(EntityManagerFactory entityManagerFactory) {
         JpaItemWriter<VideoStatistics> writer = new JpaItemWriter<>();
         writer.setEntityManagerFactory(entityManagerFactory);
         return writer;
     }
-//public class VideoStatisticsWriter implements ItemWriter<VideoStatistics> {
-
-//    private final VideoStatisticsWriteRepository videoStatisticsWriteRepository;
-//
-//    @Override
-//    public void write(Chunk<? extends VideoStatistics> chunk) throws Exception {
-//        videoStatisticsWriteRepository.saveAll(chunk.getItems());
-//    }
-
 }
